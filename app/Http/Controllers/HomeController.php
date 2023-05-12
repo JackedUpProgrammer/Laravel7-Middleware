6adfdf23f9048d8c04d0325719f00f75b433ca7b
Johan Scheepers <jhl.scheepers229@gmail.com>
2023-05-12 10:22:07 +0200

    learned about sessions as well

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,8 +22,27 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {   $user = Auth::user();
+    public function index(Request $request) // added Request $request for session injection
+    {   
+        //  $request->session->put(['johan'=>'studen']);
+        // echo $request->session()->get('johan');
+    //    return $request->session()->all(); //debugging them
+
+
+           $request->session()->forget('johan');
+           //$request->session()->flush(); deletes everything from session
+
+
+           //flash and flush is not the same //flasing data will only stay for one request
+            $request->session()->flash('message', 'Post has been created'); //when commenting this out the session will saty but with flash it wont
+            return $request->session()->get('message');
+        
+        $user = Auth::user();//this is just for name
         return view('home', compact('user'));
     }
+
+    // public function index()
+    // {   $user = Auth::user();
+    //     return view('home', compact('user'));
+    // }
 }
